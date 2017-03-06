47fb0b04796b35cf95f89e4098e07cb501fca932
xuebinqi <1120950020@qq.com>
2017-03-06 22:11:48 +0800

    test MathematicalEvaluator

--- src/main/java/project/FunOp.java
+++ src/main/java/project/FunOp.java
@@ -11,12 +11,59 @@ package project;
 public class FunOp {
     // Name of the operator.
     // For example, '+', '-' and so on.
-    public String name;
+    private String name;
 
     // The number of operands, for example, 1 or 2
     // For example, '+' needs two numbers to calculate.
-    public int arity;
+    private int arity;
 
     // The priority of this operator. We assume the default priority is -1.
-    public int priority = -1;
+    private int priority = -1;
+
+    // Get the name of the operate.
+    public String getName() {
+        return this.name;
+    }
+
+    // Get the numbers that the operate need,such as "+" need two numbers to finish the compute.
+    public int getArity() {
+        return this.arity;
+    }
+
+    // Get the priority of the operate.
+    public int getPriority() {
+        return this.priority;
+    }
+
+    // Initialize the Funop method,and set the priority of the operate.
+    public FunOp(String operate) {
+        this.name = operate;
+
+        switch (operate) {
+            case "(":
+                this.arity = 0;
+                this.priority = 0;
+                break;
+            case "+":
+            case "-":
+                this.arity = 2;
+                this.priority = 1;
+                break;
+            case "*":
+            case "/":
+                this.arity = 2;
+                this.priority = 2;
+                break;
+            case "^":
+                this.arity = 2;
+                this.priority = 3;
+                break;
+            case ")":
+                this.arity = 0;
+                this.priority = 0;
+                break;
+            default:
+                break;
+        }
+    }
 }
--- src/main/java/project/MathematicalEvaluator.java
+++ src/main/java/project/MathematicalEvaluator.java
@@ -1,9 +1,105 @@
 package project;
 
+import java.util.Stack;
+
 /**
- * Coded by Fengdingwen(2016229064)
+ * Coded by Qixuebin(2016229075) and Dingqixuan(2016229062)
  * These codes are used to do the evaluation of a given mathematical expression
  * which will be provided with form of a token queue.
  */
 public class MathematicalEvaluator {
+    private Tokenizer tokenizer;
+
+    private double result;
+
+    public MathematicalEvaluator(Tokenizer t) {
+        this.tokenizer = t;
+    }
+
+    // Judge if the expression is legal.
+    public void JudegTheExpression()
+            throws TokenException, GeneralErrorException {
+
+        // The operate stack is used to store the operate.
+        Stack<FunOp> theOperateStack = new Stack<FunOp>();
+
+        // The number stack is used to store the number.
+        Stack<Double> theValueStack = new Stack<Double>();
+
+        while (this.tokenizer.hasNextToken()) {
+            Token getTheNextToken = this.tokenizer.peekNextToken();
+
+            if (getTheNextToken.isNumber()) {
+                theValueStack.push(getTheNextToken.getNumber());
+            } else if (getTheNextToken.isIdentifier()) {
+                //    theValueStack.push(getTheNextToken.getString());
+            } else if (getTheNextToken.isDelimiter()) {
+                FunOp GetTheFunOp = new FunOp(getTheNextToken.getDelimiter());
+                if (GetTheFunOp.getName() == "(") {
+                    theOperateStack.push(GetTheFunOp);
+                } else {
+                    while (!theOperateStack.peek().getName().equals("(")) {
+                        String theOperate = theOperateStack.pop().getName();
+                        double firstValue = theValueStack.pop();
+                        double secondValue = theValueStack.pop();
+                        double theResult = ComputeTheResult(firstValue, secondValue, theOperate);
+                        theValueStack.push(theResult);
+                    }
+
+                    theOperateStack.pop();
+                }
+            } else if (getTheNextToken.isOperator()) {
+                FunOp thisOperate = new FunOp(getTheNextToken.getOperator());
+                FunOp formerOperate = theOperateStack.peek();
+                while (!theOperateStack.empty() && (formerOperate.getPriority() >= thisOperate.getPriority())) {
+                    String theOperate = theOperateStack.pop().getName();
+                    double firstValue = theValueStack.pop();
+                    double secondValue = theValueStack.pop();
+                    double theResult = ComputeTheResult(firstValue, secondValue, theOperate);
+                    theValueStack.push(theResult);
+                }
+
+                theOperateStack.push(thisOperate);
+            }
+
+        }
+
+        while (!theOperateStack.empty()) {
+            theOperateStack.pop();
+            String theOperate = theOperateStack.pop().getName();
+            double firstValue = theValueStack.pop();
+            double secondValue = theValueStack.pop();
+            double theResult = ComputeTheResult(firstValue, secondValue, theOperate);
+            theValueStack.push(theResult);
+        }
+
+        this.result = theValueStack.pop();
+    }
+
+    public double ComputeTheResult(double firstValue, double secondValue, String theOperate)
+            throws GeneralErrorException {
+        double theResult = 0;
+        switch (theOperate) {
+            case "+":
+                return firstValue + secondValue;
+            case "-":
+                return firstValue + secondValue;
+            case "*":
+                return firstValue * secondValue;
+            case "/":
+                if (secondValue == 0) {
+                    String error = "division by zero";
+                    throw new GeneralErrorException(error);
+                } else {
+                    return secondValue / firstValue;
+                }
+            case "^":
+                for (int i = 1; i <= secondValue; i++) {
+                    theResult *= firstValue;
+                }
+                return theResult;
+            default:
+                return 0;
+        }
+    }
 }
